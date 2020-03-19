a96d11ad04279501de97b5442d133ed9157f1046
jeffascript <ofoaro.jeff@gmail.com>
2020-03-19 01:53:40 +0100

    redux_fix

--- src/Reducers/userReducer.js
+++ src/Reducers/userReducer.js
@@ -1,16 +1,11 @@
 export default function (state = {}, action) {
     switch (action.type) {
         case "SET_USER": 
-            return {
-                ...state,
-                user: action.payload
-            }
+            return  action.payload
+            
 
             case "UNSET_USER":
-                return {
-                    ...state,
-                   user: undefined
-                }
+                return undefined
 
                 default:
                     return state
